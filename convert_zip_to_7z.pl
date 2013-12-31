1a085e13f29944889c074c31a2a94a78c429efe4
Paul Trost <paul.trost@cpanel.net>
2013-12-31 12:29:47 -0600

    Code cleanup and corrections

--- convert_zip_to_7z.pl
+++ convert_zip_to_7z.pl
@@ -28,28 +28,34 @@ use strict;
 use warnings;
 use File::Path qw(rmtree);
 
-# This script will convert zip files into 7z files. There is no license, it's in the public domain.
-
 # Accept list of names from what is passed to it from the shell
-# If filelist is specified (*.zip ?) then exit
-if (!@ARGV) {
-	die "Please specify a filename(s)\n";
-}
+die "No valid .zip name given, please specify a filename(s)\n"
+  if ( !@ARGV or $ARGV[0] =~ /\.7z/ );
+
+die "No .zip files found in directory\n"
+  if ( $ARGV[0] =~ /\*.zip/ );
 
 # Temp directory that will be used for zip files manipulation
 my $tmpdirectory = 'convert_tmp';
 
+# Verify that any necessary third party programs are installed
+die "Aborting, 7zip is not installed!\n"
+  if ( !-e '/usr/bin/7z' and !-e '/usr/local/bin/7z' );
+die "Aborting, unzip is not installed!\n"
+  if ( !-e '/usr/bin/unzip' and !-e '/usr/local/bin/unzip' );
+
 # For each file, make the temp firectory, unzip the zip, and archive the 7z
+print "\n";
 foreach my $file (@ARGV) {
     mkdir $tmpdirectory;
     my ( $filename, $extn ) = split( '.zip', $file );
-    $extn = substr( $file , -3 );
+    $extn = substr( $file, -3 );
     print "Uncompressing $file\n";
     chdir $tmpdirectory;
     qx(unzip "../$file" &>/dev/null);
     die "Could not uncompress $file: $!" if $? != 0;
     print "Compressing $filename.7z\n";
-    qx(7z a "$filename.7z" &>/dev/null);
+    qx(7z a "$filename.7z" \* &>/dev/null);
     die "Could not compress $filename: $!" if $? != 0;
     rename "$filename.7z", "../$filename.7z";
     chdir '../';
